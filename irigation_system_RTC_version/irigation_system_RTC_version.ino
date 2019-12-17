cd8456eb66d5f5d30bc42b9ab299541276a7f6b6
blinkmaker <blinkmaker01@gmail.com>
2019-12-17 21:07:59 +0000

    cleaned code

--- irigation_system_RTC_version/irigation_system_RTC_version.ino
+++ irigation_system_RTC_version/irigation_system_RTC_version.ino
@@ -1,22 +1,21 @@
 //Automated Arduino-based pump relay control using DS1302 Real Time Clock Module with Battery Backup
 //--------------------------------------------------------------------------------------------------
 
-#include <virtuabotixRTC.h> //DS1302 RTC library
+#include <virtuabotixRTC.h> //DS1302 RTC module library
 
 // Creation of the Real Time Clock Object
 virtuabotixRTC myRTC(7, 6, 5);  //Wiring of the RTC (CLK,DAT,RST)
 
 int timeDiff, lastReadHour; //declaring variables for time difference and previous time recorded to calculate time elapsed
-int pumpRelayTime = 30000; //pump operation time in ms
-int pumpRelay   = 8;
+int pumpRelayTime = 30000; //set pump operation time in ms
+int pumpRelay = 8;
 
 void setup() {
   Serial.begin(9600); //open serial port and set the baud rate
-  Serial.println("Power ON. Serial Monitor initiated. RTC version.");
-  pinMode(pumpRelay, OUTPUT);
+  Serial.println("Automated pump relay control (RTC version)");
   
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pumpRelay, OUTPUT); // initialise digital pin pumpRelay as an output
+  pinMode(LED_BUILTIN, OUTPUT); // initialise digital pin LED_BUILTIN as an output
   
   // Set the current date, and time in the following format:
   // seconds, minutes, hours, day of the week, day of the month, month, year
@@ -43,7 +42,6 @@ void setup() {
 }
 
 void loop() {
-      
   myRTC.updateTime(); // update of variables for time or accessing the individual elements.                                                                                 
 
 /*
@@ -73,16 +71,16 @@ void loop() {
       Serial.print("Hour difference: "); //display the hour difference for debugging
       Serial.println(timeDiff);
       
-    if (timeDiff >= 1) //conditional check if at least one hour difference
-    {                                                                                
+      if (timeDiff >= 1) //conditional check if at least one hour difference
+      {                                                                                
         digitalWrite(pumpRelay, HIGH);  //turns on the pump relay
-        digitalWrite(LED_BUILTIN, HIGH);   //turns on the on-board LED pin on Arduino UNO board, used for testing
-        Serial.println("*** Pump turned ON for 40 secs ***");
-        delay(pumpRelayTime); //delay of 40 seconds by default
-        digitalWrite(pumpRelay, LOW);
-        digitalWrite(LED_BUILTIN, LOW);  //turns off the on-board LED pin on Arduino UNO board, used for testing
+        digitalWrite(LED_BUILTIN, HIGH);   //turns on the on-board LED (used for testing)
+        Serial.println("*** Pump turned ON ***");
+        delay(pumpRelayTime); //pump relay operation time
+        digitalWrite(pumpRelay, LOW); //turns off the pump relay
+        digitalWrite(LED_BUILTIN, LOW);  //turns off the on-board LED (used for testing)
         Serial.println("*** Pump turned OFF ***");                                                                      
-    }                                                                                                
-    lastReadHour = myRTC.hours; //stores current hour value as previous hour value                                                                           
+      }    
+      lastReadHour = myRTC.hours; //stores current hour value as previous hour value                                                   
   }
 }
