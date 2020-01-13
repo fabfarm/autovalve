eb6531c8e0059ffaea1271f208c4311358604ffe
Shakeel <blinkmaker01@gmail.com>
2020-01-13 12:35:26 +0000

    updated code

--- Measuring_AC_Current_Using_ACS712/Measuring_AC_Current_Using_ACS712.ino
+++ Measuring_AC_Current_Using_ACS712/Measuring_AC_Current_Using_ACS712.ino
@@ -1,15 +1,15 @@
 /*
 * Measuring AC Current Using ACS712
 */
-const int ACsensorPin = A0;
+const int ACSpin = A0;
 const int mVperAmp = 100; // Output sensitivity in mV per Amp
 // Use 185 for 5A module, 100 for 20A module and 66 for 30A module
 
 double VPP = 0; // peak-to-peak voltage
 double VRMS = 0;  // RMS voltage
-double AmpsRMS = 0; // RMS current
+double IRMS = 0; // RMS current
 
-float ACSoffset = 0.025; // set quiescent Vrms output voltage
+float VRMSoffset = 0.025; // set quiescent Vrms output voltage
 //voltage at an output terminal with reference to a common terminal, normally ground, 
 //when no signal is applied to the input.
 
@@ -19,17 +19,18 @@ void setup(){
 
 void loop(){
   VPP = getVPP(); // find peak-to-peak voltage
-  VRMS = ((VPP / 2.0) * 0.707) - ACSoffset; // divide by 2 to get peak voltage. 1 ÷ √(2) is 0.707
-  AmpsRMS = (VRMS * 1000) / mVperAmp; // first, multiply by 1000 to convert to mV
+  VRMS = ((VPP / 2.0) * 0.707) - VRMSoffset; // divide by 2 to get peak voltage. 1 ÷ √(2) is 0.707
+  IRMS = (VRMS * 1000) / mVperAmp; // first, multiply by 1000 to convert to mV
   
   Serial.print("Vpp: ");
   Serial.print(VPP, 3);// print to 3 decimal places
   Serial.print("\tVrms: ");
   Serial.print(VRMS, 3);// print to 3 decimal places
   Serial.print("\tIrms: ");
-  Serial.println(AmpsRMS, 3);// print to 3 decimal places
+  Serial.println(IRMS, 3);// print to 3 decimal places
 }
 
+// function to measure peak-to-peak voltage
 float getVPP()  // continously sampling and logging max and min values
 {
   float result;
@@ -41,7 +42,7 @@ float getVPP()  // continously sampling and logging max and min values
   uint32_t start_time = millis();
   while((millis()- start_time) < 1000) // sample for 1000 ms or 1 sec
   {
-    readValue = analogRead(ACsensorPin);
+    readValue = analogRead(ACSpin);
     // check if a new maxValue
     if (readValue > maxValue)
     {
